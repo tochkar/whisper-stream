88e83d6c44bb7793ad995620c2261654d3d76e42
tochkar <55049700+tochkar@users.noreply.github.com>
2025-04-23 21:55:03 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,8 +1,13 @@
+import os
+from dotenv import load_dotenv
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
+load_dotenv()
+HF_TOKEN = os.environ["HF_TOKEN"]  # ваш Hugging Face токен
+
 model_name = "IlyaGusev/saiga-mistral-7b-qlora"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=HF_TOKEN)
+model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=HF_TOKEN)
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=128)
 
 def extract_addresses(text):
@@ -18,5 +23,7 @@ def extract_addresses(text):
     output = pipe(prompt)[0]['generated_text']
     return output
 
-# Пример
-print(extract_addresses("Меня заберите, пожалуйста, с улицы Лебедева, 12, а везите на Короля 21"))
+# Тест!
+if __name__ == "__main__":
+    test_text = "Меня заберите, пожалуйста, с улицы Лебедева, 12, а везите на Короля 21"
+    print(extract_addresses(test_text))
